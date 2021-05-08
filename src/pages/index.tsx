4d679f2806ac6b3193227557a1f81d5f15de97b2
Joao Melo <jopcmelo@gmail.com>
2021-05-08 12:28:22 -0300

    feat: add Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>Hello world!</h1>
+      <main className={styles.main}>
+        <section className={styles.section}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the plublications <br />
+            <span>for $9.99 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
