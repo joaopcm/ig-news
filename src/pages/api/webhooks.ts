0f09a4fcc6d10156495a99d64ae6a79291a83448
Joao Melo <jopcmelo@gmail.com>
2021-05-08 16:44:02 -0300

    fix: remove useless event

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -22,7 +22,6 @@ export const config = {
 
 const relevantEvents = new Set([
   "checkout.session.completed",
-  "customer.subscription.created",
   "customer.subscription.updated",
   "customer.subscription.deleted",
 ]);
