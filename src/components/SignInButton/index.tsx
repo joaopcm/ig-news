501dfa79132da6d59ba2d69fb8c342fc2bfcbd42
Joao Melo <jopcmelo@gmail.com>
2021-05-08 14:05:48 -0300

    feat: add authentication with Github and NextAuth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,25 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.button}>
+  return session ? (
+    <button type="button" className={styles.button} onClick={() => signOut()}>
       <FaGithub color="#04d361" />
-      joaopcm
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.button}>
+    <button
+      type="button"
+      className={styles.button}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
-      Sig in with Github
+      Sign in with Github
     </button>
   );
 }
